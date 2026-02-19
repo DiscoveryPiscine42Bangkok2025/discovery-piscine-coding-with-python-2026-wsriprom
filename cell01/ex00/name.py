c2ad975e3ae31a1dcfbcfedb3be6033a05e57103
wsriprom <68030256@kmitl.ac.th>
2026-02-19 08:15:41 +0000

    love python

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Worawet"
+last_name = "Sriprom"
+print(first_name + last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Worawet"
+last_name = "sriprom"
+whole_name = first_name + " " + last_name
+print(whole_name)
